8f7be85b4b6c8121762777a37e92bdb64cfd5b5f
Seethalaksmi <seetha.laksmi@yahoo.com>
2019-05-26 00:00:10 +0200

    Arrays

---
+++ Ex6-arrays.js
@@ -0,0 +1,12 @@
+//Declare an empty array
+let myFavoriteBooks=[]
+console.log("The value of myFavoriteBooks is: an empty array \'[]\'")
+console.log(myFavoriteBooks)
+
+//Declare an array with my favorite animals inside
+let myFavoriteAnimals=["Dog", "Elephant", "Monkey"]
+console.log(myFavoriteAnimals)
+
+//append 'baby pig' to the my favorite array declared above
+myFavoriteAnimals.push('baby pig')
+console.log(myFavoriteAnimals)
