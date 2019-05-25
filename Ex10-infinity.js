32d8852341417ade1515354a7e7a53459c478361
Seethalaksmi <seetha.laksmi@yahoo.com>
2019-05-26 00:02:40 +0200

    Multiple types in an array & compare infinity

---
+++ Ex10-infinity.js
@@ -0,0 +1,11 @@
+let myArray = [10, 6, 12, 'dog', 'elephant']
+console.log("The value of myArray is: " + myArray) // can store elements of diiferent type in array
+let a = 6 / 0
+let b = 10 / 0
+
+if (a === b) {
+    console.log('The value of a and b is same: ' + a + ' ' + b) // both infinity are same
+}
+else {
+    console.log('They are not same')
+}
