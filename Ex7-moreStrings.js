52788f1116ef8b12734987dc61862b4dc2ad9eab
Seethalaksmi <seetha.laksmi@yahoo.com>
2019-05-26 00:01:08 +0200

    More Strings -Length

---
+++ Ex7-moreStrings.js
@@ -0,0 +1,6 @@
+let myString = "this is a test"
+console.log(myString)
+//get the length of mystring
+let lengthOfmyString = myString.length
+//print the length of myString
+console.log(lengthOfmyString)
