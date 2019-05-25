e89d15f9c3e8bd22f0dabc9ce974cc921abf31a8
Seethalaksmi <seetha.laksmi@yahoo.com>
2019-05-26 00:02:02 +0200

    Modulus

---
+++ Ex9-modulus.js
@@ -0,0 +1,16 @@
+let x=7
+x=x%3
+//here % is the remainder of 7 when divided by 3 which is 1.
+console.log(x)
+
+x=8
+x=x%3
+//here the remainder of  8 divided by 3 is 2
+console.log(x)
+
+x=9
+x=x%3
+//here the remainder of 9 when divided by 3 is 0
+console.log(x)
+
+
