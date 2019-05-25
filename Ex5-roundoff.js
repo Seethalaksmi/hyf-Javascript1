1585856053e2ae1f700fa10e6b0de18740ef50be
Seethalaksmi <seetha.laksmi@yahoo.com>
2019-05-25 23:59:54 +0200

    roundoff number

---
+++ Ex5-roundoff.js
@@ -0,0 +1,11 @@
+//Declare z and assign it 7.25
+let z = 7.25
+console.log(z)
+
+//Declare a and roundoff z to the nearest integer and assign it to variable a
+let a = Math.round(z)
+console.log(a)
+
+//Declare b and assign it the maximum of a & z
+let b = Math.max(z,a)
+console.log(b)
