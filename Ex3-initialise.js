0d1e588c0036dfc19b83dbed8561f60dfe5aaa55
Seethalaksmi <seetha.laksmi@yahoo.com>
2019-05-25 23:58:55 +0200

    initialisze variable

---
+++ Ex3-initialise.js
@@ -0,0 +1,13 @@
+//declaring my variable x
+let x;
+
+// before initialising my variable x
+console.log("The value of my variable x will be: undefined")
+console.log(x)
+
+//initialising my variable x
+x=3
+
+// after initialising my variable x
+console.log("Now the value of my variable x will be: 3")
+console.log("Now the actual value of my variable x is: ", x)
